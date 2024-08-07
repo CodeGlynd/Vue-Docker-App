612506f23e8b9c6178bde2f878f08a86fbd796ea
unknown <ali405930@fci.bu.edu.eg>
2024-08-07 23:24:07 +0300

    build: src js files

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+<div class="container">
+<Header title="Vue App" action="Get Started" color="lightblue" bgColor="darkblue"/>
+
+
+</div>
+ 
+<div class="container">
+<Header title="ToDo List" action="Add Task"/>
+
+</div>
+
+</template>
+
+<script>
+export default {
+    name: 'App',   
+    components:{
+        Header
+    }
+    }
+
+import Header from './Header.vue';
+
+
+</script>
+
+<style>
+    
+#app{
+    text-align:center;
+    margin-top:60px;
+}
+
+.container{
+    border: 1px solid steelblue;
+    padding: 30px;
+    max-width: 500px;
+    margin:20px auto;
+}
+
+
+</style> 
